ee2f15af229694470766addfae1864edbede3985
kirillche <kiriciri@gmail.com>
2019-12-22 11:30:56 +0300

    default.html

--- backend/webapp/views.py
+++ backend/webapp/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
-    return render(request, 'webapp/index.html', {})
+    return TemplateResponse(request, ['webapp/index.html', 'webapp/default.html'])
+    # return render(request, 'webapp/index.html', {})
 
 
 @csrf_exempt
